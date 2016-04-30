859d4e7de7936f36349fa3073317dd0a3bde182a
ostap_moravskyi <ostap.moravskyi@gmail.com>
2016-04-30 17:14:59 +0300

    new project with MVS

--- CreditCard.cs
+++ CreditCard.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
-namespace Kottans.Task
+
+namespace CreditCardNumber
 {
     public static class CreditCard
     {
@@ -90,33 +91,6 @@ namespace Kottans.Task
             catch
             {
                 return false;
-            }
-        }
-    }
-    public class Program
-    {
-        // Testing result
-        public static void Main()
-        {
-            try
-            {
-                string number = "4561 2612 1234 5467";
-                // Find out the vendor of the credit card
-                string vendor = CreditCard.GetCreditCardVendor(number);
-                Console.WriteLine(vendor);
-                // Find out whether credit card number is valid (according to Luhn algorithm) or not
-                bool isValid = CreditCard.IsCreditCardNumberValid(number);
-                Console.WriteLine(isValid);
-                // Generate next valid number for credit card 
-                string nextNumber = CreditCard.GenerateNextCreditCardNumber(number);
-                Console.WriteLine(nextNumber);
-
-                Console.ReadLine();
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex);
-                Console.ReadLine();
             }
         }
     }
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CreditCardNumber;
+
+namespace Kottans.Task
+{
+    public class Program
+    {
+        // Testing result
+        public static void Main()
+        {
+            try
+            {
+                string number = "4561 2612 1234 5467";
+                // Find out the vendor of the credit card
+                string vendor = CreditCard.GetCreditCardVendor(number);
+                Console.WriteLine(vendor);
+                // Find out whether credit card number is valid (according to Luhn algorithm) or not
+                bool isValid = CreditCard.IsCreditCardNumberValid(number);
+                Console.WriteLine(isValid);
+                // Generate next valid number for credit card 
+                string nextNumber = CreditCard.GenerateNextCreditCardNumber(number);
+                Console.WriteLine(nextNumber);
+
+                Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+        }
+    }
+}
